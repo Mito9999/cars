ab2e6577087188bb34557a1f177917375b966097
Mito6211 <willmoore674@gmail.com>
2021-03-13 09:55:35 -0500

    Added error handling, express

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
-const puppeteer = require("puppeteer");
+import puppeteer from "puppeteer";
+import express from "express";
 
-(async () => {
+const app = express();
+
+const formatDate = (dateString) => {
+  try {
+    const date = new Date(dateString);
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const month = date.getMonth();
+    const day = date.getDay();
+    const year = date.getFullYear();
+
+    return `${hours}:${minutes} ${month}/${day}/${year}`;
+  } catch {
+    console.log("Invalid Date passed to formatDate()");
+    return dateString;
+  }
+};
+
+app.get("/:location", async (req, res) => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  try {
+    const { location } = req.params || "sfbay";
+    const page = await browser.newPage();
 
-  await page.goto(
-    "https://norfolk.craigslist.org/d/cars-trucks-by-owner/search/cto"
-  );
+    await page.goto(
+      `https://${location}.craigslist.org/d/cars-trucks-by-owner/search/cto`
+    );
 
-  const posts = await page.evaluate(() => {
-    let elements = Array.from(document.querySelectorAll(".rows > *"));
-    return elements.map((post) => ({
-      url: post.children[0].href,
-      date: post.children[1].children[1].attributes.datetime.value,
-      title: post.children[1].children[2].children[0].innerText,
-      price: post.children[1].children[3].children[0].innerText,
-    }));
-  });
-
-  console.log(posts);
+    try {
+      const posts = await page.evaluate(() => {
+        let elements = Array.from(document.querySelectorAll(".rows > *"));
+        return elements.map((post) => ({
+          url: post.children[0].href,
+          date: post.children[1].children[1].attributes.datetime.value,
+          title: post.children[1].children[2].children[0].innerText,
+          price: post.children[1].children[3].children[0].innerText,
+        }));
+      });
+      res.json({
+        message: "Successfully retrieved posts",
+        posts,
+      });
+    } catch {
+      res.status(404).json({
+        message: "Failed to evaluate posts",
+        posts: [],
+      });
+    }
+  } catch {
+    res.status(500).json({
+      message: "An unknown server error occured",
+      posts: [],
+    });
+  }
 
   await browser.close();
-})();
+});
+
+app.listen(2000, () => console.log(`http://localhost:2000/`));
