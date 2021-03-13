57b0ce05da85eb1b8d423d883976f3f34d8e0b72
Mito6211 <willmoore674@gmail.com>
2021-03-13 16:54:05 -0500

    Added ability for starting and stopping the feed of posts

--- bot.js
+++ bot.js
@@ -10,16 +10,32 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const timeoutIDs = [];
+const data = { ...mockPosts };
+const postsShown = [];
+let i = 0;
+
 client.on("message", async (msg) => {
   if (msg.content === "!cars") {
     // const res = await fetch("http://localhost:2000/sfbay");
     // const data = await res.json();
-    const data = mockPosts;
-    data.posts.forEach((post, idx) => {
-      setTimeout(() => {
-        msg.reply(`${post.price} - ${post.title} - ${post.date} - ${post.url}`);
-      }, 3000 * idx);
+
+    // not using .forEach index parameter due to conditional timeouts
+    data.posts.forEach((post) => {
+      if (!postsShown.includes(post)) {
+        const timeoutID = setTimeout(() => {
+          msg.reply(
+            `${post.price} - ${post.title} - ${post.date} - ${post.url}`
+          );
+          postsShown.push(post);
+        }, 3000 * i);
+        i++;
+        timeoutIDs.push(timeoutID);
+      }
     });
+  } else if (msg.content === "!stop") {
+    i = 0;
+    timeoutIDs.forEach((id) => clearTimeout(id));
   }
 });
 
